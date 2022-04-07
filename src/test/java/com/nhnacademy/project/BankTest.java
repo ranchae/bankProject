91c54503892a40f998c14600107178cab2967a7c
ranchae <orchid59@naver.com>
2022-04-07 10:08:45 +0900

    1단계 성공

--- src/test/java/com/nhnacademy/project/BankTest.java
+++ src/test/java/com/nhnacademy/project/BankTest.java
@@ -1,13 +1,25 @@
 package com.nhnacademy.project;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankTest {
+    private Bank bank;
+
+    @BeforeEach
+    void setUp() {
+        bank = new Bank();
+    }
 
     @Test
+    @DisplayName("입금테스트")
     void deposit() {
-
+        double wonLeft = 1_000;
+        double wonRight = 1_000;
+        double result = bank.deposit(1_000,1_000);
+        assertEquals(2_000, result);
     }
 }
